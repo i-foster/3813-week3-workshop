6461e3f878ba9b687dbe8d85257f93bf73d10cdf
i-foster <nakor27@live.com>
2024-07-28 15:05:30 +1000

    added gitignore, server and index files

---
+++ week3_server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app= express();
+var http = require('http').Server(app)
+
+
+app.use(express.static(__dirname + '/www'));
+
+let server = http.listen(3000, function(){ 
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("my first node server!");
+    console.log("server listerning on: " + host + 
+        "port: " + port)
+})
+
+app.get('/test', function (req, res) {
+    res.sendFile(__dirname + '/www/test.html');
+    });
